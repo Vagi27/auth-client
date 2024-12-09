5dcbb0a0660aa2109b61469ee16af2c6809ea2c9
Vagish <Vagishmlk@gmail.com>
2024-12-09 18:39:50 +0530

    app set

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-    return <div>app</div>;
+    return (
+        <div>
+            <Outlet />
+        </div>
+    );
 };
 
 export default App;

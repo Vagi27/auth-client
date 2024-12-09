03549442901654b1dde65104f8014d52fef35afd
Vagish <Vagishmlk@gmail.com>
2024-12-09 18:40:13 +0530

    router set

---
+++ src/router.jsx
@@ -0,0 +1,24 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Navigate,
+    Route,
+} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import App from "./App.jsx";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route path="home" element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+
+            <Route path="*" element={<Navigate to="home" />} />
+        </Route>
+    )
+);
+
+export default router;

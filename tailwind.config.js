c16e14275eec65425e31ac45cecdae533d4cd785
Vagish <Vagishmlk@gmail.com>
2024-12-10 18:09:44 +0530

    tailwind configure

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",  // Tailwind will purge unused styles from these files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
